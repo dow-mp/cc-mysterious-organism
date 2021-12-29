c2878bf6c10769982d0e88d3d69578f5a54fad35
dow-mp <derin.mitchell@gmail.com>
2021-12-29 09:17:03 -0500

    Completed fxs compareDNA and willLikelySurvive; tested & functional

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ const returnRandomBase = () => {
 };
 
 // Returns a random single stand of DNA containing 15 bases (mockUpStrand() is used to generate an array containing 15 bases to represent a single DNA strand with 15 bases.)
-const newStrand = [];
+
 const mockUpStrand = () => {
+  const newStrand = [];
   for (let i = 0; i < 15; i++) {
     newStrand.push(returnRandomBase())
   };
-  console.log(newStrand);
   return newStrand
 };
 
@@ -19,41 +19,68 @@ const mockUpStrand = () => {
 /*.mutate() is responsible for randomly selecting a base in the object’s dna property and changing the current base to a different base. Then .mutate() will return the object’s dna.
 
 For example, if the randomly selected base is the 1st base and it is 'A', the base must be changed to 'T', 'C', or 'G'. But it cannot be 'A' again. */
-let randomNum = Math.floor(Math.random()*1000);
-mockUpStrand();
-const pAequorFactory = (randomNum, newStrand) => {
+function randomNum() {
+  return Math.floor(Math.random()*1000);
+};
+
+const pAequorFactory = (randomNum, dnaArray) => {
   return {
     specimenNum: randomNum,
-    dna: newStrand,
+    dna: dnaArray,
     mutate() {
       let i = Math.floor(Math.random()*15);
       console.log(i);
       let randomBase = returnRandomBase();
-      let currentBase = newStrand[i];
+      let currentBase = this.dna[i];
       if (randomBase != currentBase) {
         this.dna.splice(i,1,randomBase);
-      } else if (randomBase = newStrand[i]) {
+      } else if (randomBase = this.dna[i]) {
         randomBase = returnRandomBase(); //what if this returns the same randomBase again - add value of newStrand[i] as a var and set if statement so that randomBase != this variable?
         this.dna.splice(i,1,randomBase);
       } 
         return this.dna;
     },
-    compareDNA(obj) {
-      for (i=0; i<obj.length; i++) {
-        //NEED JS NOTEBOOK FOR EXAMPLE OF ARRAY COMPARISON USING FOR LOOP - believe you compare each item with a for loop within a for loop and then .push to a new array for each matching element
+    compareDNA(otherObj) {
+      const mutualDNA = [];
+      let count = 0;
+      for (let i=0; i<15; i++) {
+        if (otherObj.dna[i] === this.dna[i]) {
+          mutualDNA.push(this.dna[i]);
+          count++
+        }
+      };
+      //console.log(otherObj.dna, this.dna)
+      //console.log("Specimen 1 and specimen 2 have ", (count/15)*100, "% DNA in common.");
+      return mutualDNA;
+    },
+    willLikelySurvive() { 
+      let count = 0;
+      for (let i=0; i<15; i++) {
+        if (this.dna[i] === "C" || this.dna[i] === "G") {
+          count++;
+        };
+      };
+      //console.log((count/15)*100);
+      if (((count/15)*100) >= 60) {
+      return true
+      } else {
+        return false
       }
-      /*.compareDNA() has one parameter, another pAequor object.
-
-    The behavior of .compareDNA() is to compare the current pAequor‘s .dna with the passed in pAequor‘s .dna and compute how many bases are identical and in the same locations. .compareDNA() does not return anything, but prints a message that states the percentage of DNA the two objects have in common — use the .specimenNum to identify which pAequor objects are being compared.*/
     }
   }
 }
 
-const newOrgA = pAequorFactory(randomNum, newStrand);
+const newOrgA = pAequorFactory(randomNum(), mockUpStrand());
+const newOrgB = pAequorFactory(randomNum(), mockUpStrand());
 
 //console.log(newOrgA);
+//console.log(newOrgB);
 
-console.log(newOrgA.mutate());
+//console.log(newOrgA.mutate());
+//console.log(newOrgA.compareDNA(newOrgB));
+
+//console.log(newOrgA.willLikelySurvive());
+//console.log(newOrgB.willLikelySurvive());
 
 
 
